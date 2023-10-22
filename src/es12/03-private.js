75a72495f4e44c66071b4358619a8e9b2723dff8
Santiago <santiarango90@gmail.com>
2023-10-21 21:25:25 -0500

    ES12 nuevos archivos

---
+++ src/es12/03-private.js
@@ -0,0 +1,22 @@
+//declaracion de clases
+class Rectangulo {
+  constructor(base, altura) {
+    this.base = base;
+    this.altura = altura;
+  }
+  // # sirve para hacer privado el metodo
+  #area(base,altura){
+    return `"El area del Rectangulo es: ${base*altura} y se obtiene multiplicando la base ${base} por la altura ${altura}`;
+  }
+  get #laAltura(){
+    return this.altura
+  }
+  set #laAltura(a){
+    return this.altura = n;
+  }
+};
+
+const areaRectangulo = new Rectangulo();
+console.log(areaRectangulo.area(4,4));
+console.log(`la nueva altura es: ${altura = 10}`);
+
